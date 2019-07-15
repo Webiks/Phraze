083797056574ca169ec35ef7b53d5388fe4ff94f
TheBigR <nevoroy@gmail.com>
2019-07-15 16:37:19 +0300

    adding base components - map, searchPage and controlBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ControlBarComponent } from './components/control-bar/control-bar.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { MapComponent } from './components/map/map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlBarComponent,
+    SearchPageComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/control-bar/control-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-bar',
+  templateUrl: './control-bar.component.html',
+  styleUrls: ['./control-bar.component.scss']
+})
+export class ControlBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
